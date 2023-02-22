c08bd4f7920f59ac3ca0feb764559465b1ba70b2
xDavide9 <biancodavidetech@gmail.com>
2023-02-22 17:59:41 +0100

    Bootstrap project with Spring Web, Spring Security and Lombok dependencies; create a planning.txt

---
+++ src/test/java/com/xdavide9/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdavide9.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
